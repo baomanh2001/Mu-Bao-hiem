fccd16d2fc581ce9a551f049dd60b222607269ed
baomanh2001 <75458250+baomanh2001@users.noreply.github.com>
2021-11-07 13:14:38 +0700

    Update Text_Nhan_RF24.ino

--- Text_Nhan_RF24/Text_Nhan_RF24.ino
+++ Text_Nhan_RF24/Text_Nhan_RF24.ino
@@ -6,92 +6,105 @@ const byte addresses[][6] = {"00001", "00002"};
 #define Relay 8
 #define Dong 4
 
-
 unsigned long currentMillis;
 unsigned long prevMillis = 0;
-unsigned long txIntervalMillis = 300; 
+unsigned long txIntervalMillis = 300;
 char ans[1];
-const char test[]="1";
+const char test[] = "1";
 int flag = 0;
 
-enum State{ChuaCoMu, DaCaiMu}; 
-State state ;
+enum State
+{
+  ChuaCoMu,
+  DaCaiMu
+};
+State state;
 
 void setup()
 {
-  pinMode(Relay,OUTPUT);
-  pinMode(Dong,INPUT);
+  pinMode(Relay, OUTPUT);
+  pinMode(Dong, INPUT);
   Serial.begin(9600);
   Serial.println("READY..........");
   radio.begin();
+
+  radio.setChannel(1);
   //radio.openWritingPipe(addresses[0]); // 00001
- radio.openReadingPipe(1, addresses[1]); // 00002
- 
+  radio.openReadingPipe(1, addresses[1]); // 00002
+
   radio.setPALevel(1);
   state = ChuaCoMu;
-   Serial.println("READY..........");  
+  Serial.println("READY..........");
   radio.startListening();
-    if(radio.available())
+  if (radio.available())
   {
-    
-    radio.read(&ans,1);
+
+    radio.read(&ans, 1);
     //Serial.println(ans[0]);
   }
- switch(ans[0]){
+  switch (ans[0])
+  {
   case '1':
-  Serial.println("nhan 1");
-  flag = 1;
-  break;
- }
-}
-
-void loop(){
-  radio.startListening();
-  currentMillis = millis();
-  if (currentMillis - prevMillis >= 500) 
-  {
-    flag = 0;
-    Serial.println("Vao Xet");
-      if(radio.available())
-        {
-        Serial.println("Vao Doc");
-        radio.read(&ans,1);
-        
-        Serial.print("Ans la:");
-        //Serial.println(ans[0]);
-     switch(ans[0]){
-        case '1':
-        Serial.println("nhan 1");
-        flag = 1;
-        break; 
-     }}
-   prevMillis = millis();
+    Serial.println("nhan 1");
+    flag = 1;
+    break;
   }
-  else
+}
+void XetMu()
+{
+  Serial.println("Vao Doc");
+  radio.read(&ans, 1);
+  Serial.print(ans[0]);
+  Serial.println();
+  switch (ans[0])
   {
-Serial.println(state);
-  switch(state)
+  case '1':
+    Serial.println("nhan 1");
+    flag = 1;
+    break;
+  }
+}
+void loop()
+{
+  radio.startListening();
   {
+    Serial.println(state);
+    switch (state)
+    {
     case ChuaCoMu:
-    if(flag == 1)
+      Serial.println("Vao Xet Khi Chua Co Mu");
+      if (radio.available())
+      {
+        XetMu();
+        prevMillis = millis();
+      }
+      if (flag == 1)
       {
         Serial.println("DaCaiMu");
         state = DaCaiMu;
-        digitalWrite(Relay,HIGH);
+        digitalWrite(Relay, HIGH);
       }
-     
-    break;
+      break;
     case DaCaiMu:
-    if(flag != 1)
-      {state = ChuaCoMu;
-        
-        digitalWrite(Relay,LOW);
+
+      Serial.println("Dang CaiMu");
+      currentMillis = millis();
+      if (currentMillis - prevMillis >= 500)
+      {
+        flag = 0;
+        Serial.println("Vao Xet");
+        if (radio.available())
+        {
+          XetMu();
+          prevMillis = millis();
+        }
       }
-      else
-    Serial.println("Dang CaiMu");
-    break;
-    
-  }
-  
+      if (flag != 1)
+      {
+        state = ChuaCoMu;
+        digitalWrite(Relay, LOW);
+      }
+      break;
+    }
   }
 }
